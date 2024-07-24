82f1f7a72c61704fbc34c913f2bdada49d74958b
Nihad Prakarsh <nihadprakarsh@gmail.com>
2024-07-24 17:00:50 +0530

    Made decorative changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import './Navbar.css'; // Import custom CSS
+import React from "react";
+import "./Navbar.css"; // Import custom CSS
 
-var Name = "{ NIHAD PRAKARSH }"
+var Name = "{ NIHAD PRAKARSH }";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">{Name}</div>
+      <div className="navbar-brand">
+        <a href="" target="_blank" className="href">
+          {Name}
+        </a>
+      </div>
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
+          <a className="nav-link" href="#">
+            About
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Blog</a>
+          <a className="nav-link" href="#">
+            Blog
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Timeline</a>
+          <a className="nav-link" href="#">
+            Timeline
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
+          <a className="nav-link" href="#">
+            Contact
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Resume</a>
+          <a className="nav-link" href="#">
+            Resume
+          </a>
         </li>
       </ul>
     </nav>
